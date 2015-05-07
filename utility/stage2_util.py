d790b0d6824799202625439633f466057763b50d
kalabharath <kalabharath@gmail.com>
2015-05-07 17:11:17 +1000

    Stage 2, filter the top_hits

--- utility/stage2_util.py
+++ utility/stage2_util.py
@@ -27,6 +27,17 @@ def getNextSmotif(map_route):
         if not checkFile(i):
             return i, map_route[i]
 
+def getNchiSum(pcs_filter):
+    #['PCS_filter', [[0, 0.04950933584267763, [[29.943566759232581, 7.8711499604434305], [35.840777738719297, 8.0029596961619411]]], [1, 0.001111114208027107, [[142.57986684837724, 86.977485137698238], [91.67464082627454, 54.850648632962333]]]]]
+    tensors = pcs_filter[1]
+    snchi =0
+    for tensor in tensors:
+        tag = tensor[0]
+        nchi = tensor[1]
+        axrh = tensor[2]
+        snchi += nchi
+    return snchi
+
 def makeTopPickle(previous_smotif_index):
 
     hits = []
@@ -37,18 +48,32 @@ def makeTopPickle(previous_smotif_index):
         for thit in thits:
             hits.append(thit)
     """
+                    0                                  1
      dump_log = [['smotif',['seq_filter', 'smotif_seq', 'seq_identity', "blosum62_score"],
+                                           2
                 ['contacts_filter','no_of_contacts', '%_of_contacts_observed'],
+                              3
                 ['PCS_filter', 'tensor_fits']]]
     """
     print len(hits)
-
+    new_dict={}
+    seqs = []
     for hit in hits:
         smotif = hit[0]
         seq_filter = hit[1]
-        print seq_filter
-
-
+        smotif_seq = seq_filter[1]
+        if smotif_seq not in seqs:
+            seqs.append(smotif_seq)
+            pcs_filter = hit[3]
+            nchi_sum = getNchiSum(pcs_filter)
+            new_dict.setdefault(nchi_sum, []).append(hit)
+    keys = new_dict.keys()
+    keys.sort()
+    print len(keys)
+    dump_pickle = []
+    for i in range(0,10):
+        dump_pickle.append(new_dict[keys[i]])
+    io.dumpPickle(str(previous_smotif_index)+"_tophits.pickle", dump_pickle)
     return True
 
 def getRunSeq():
