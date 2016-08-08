306e113d891bef60a511eb40d8f0e9a2d2b1689e
kalabharath <kalabharath@gmail.com>
2016-08-08 15:36:37 -0600

    Improved code that 1. No of top models per smotif is now a specified as a factor of len(smotifs_to_be_assembled) * 125. 2. Automatically quits the que when no_of_top hits ==0

--- auto_stage0.py
+++ auto_stage0.py
@@ -8,15 +8,15 @@ Date: 31/03/15 , Time:10:56 AM
 Prepare all the relavant files for stage1 & 2
 
 """
-import sys, os
+import os
+import sys
+
 sys.path.append("../../main")
-#sys.path.append('/short/xc4/kbp502/BOSS/zinr')
+# sys.path.append('/short/xc4/kbp502/BOSS/zinr')
 
 import utility.io_util    as io
 import utility.ss_util    as ss
 import utility.PCSmap     as PCSmap
-import utility.ContactMap as contact
-
 
 
 def matchSeq2SS(aa_seq, ssfile):
@@ -26,11 +26,11 @@ def matchSeq2SS(aa_seq, ssfile):
     with open(ssfile) as fin:
         lines = fin.readlines()
 
-    for i in range (0,len(lines)):
+    for i in range(0, len(lines)):
         if lines[i] == 'FORMAT %4d %1s %2d %2d %8.3f %8.3f %8.3f %4.2f %s\n':
             print lines[i]
             j = i
-            for j in range(j,len(lines)):
+            for j in range(j, len(lines)):
 
                 content = lines[j].split()
                 if len(content) == 9:
@@ -38,57 +38,50 @@ def matchSeq2SS(aa_seq, ssfile):
             break
 
     print len(aa_seq), len(raw_ss)
-    diff = len(raw_ss)-len(aa_seq)
+    diff = len(raw_ss) - len(aa_seq)
     print 'diff', diff
     if diff > 0:
-        for i in range(0,diff+1):
-            t_aa=''
-            t_ss=''
-            for j in range(i,i+len(aa_seq)):
-                t_aa=t_aa+raw_ss[j][1]
-                t_ss=t_ss+raw_ss[j][-1]
+        for i in range(0, diff + 1):
+            t_aa = ''
+            t_ss = ''
+            for j in range(i, i + len(aa_seq)):
+                t_aa = t_aa + raw_ss[j][1]
+                t_ss = t_ss + raw_ss[j][-1]
             print t_aa, t_ss
             if t_aa == aa_seq:
                 print t_aa, len(t_aa)
-                #REMARK     h-Helix    e-Strand   c-Coil (Sequence based)
-                t_ss = t_ss.replace('c','L')
-                t_ss = t_ss.replace('e','E')
-                t_ss = t_ss.replace('h','H')
+                # REMARK     h-Helix    e-Strand   c-Coil (Sequence based)
+                t_ss = t_ss.replace('c', 'L')
+                t_ss = t_ss.replace('e', 'E')
+                t_ss = t_ss.replace('h', 'H')
                 print t_ss, len(t_ss)
                 return t_ss
     else:
 
-        t_aa=''
-        t_ss=''
-        for j in range(0,len(aa_seq)):
-            t_aa=t_aa+raw_ss[j][1]
-            t_ss=t_ss+raw_ss[j][-1]
-        t_ss = t_ss.replace('c','L')
-        t_ss = t_ss.replace('e','E')
-        t_ss = t_ss.replace('h','H')
+        t_aa = ''
+        t_ss = ''
+        for j in range(0, len(aa_seq)):
+            t_aa = t_aa + raw_ss[j][1]
+            t_ss = t_ss + raw_ss[j][-1]
+        t_ss = t_ss.replace('c', 'L')
+        t_ss = t_ss.replace('e', 'E')
+        t_ss = t_ss.replace('h', 'H')
         print t_ss, len(t_ss)
         return t_ss
 
 
-
-
-
 data = io.readInputDataFiles('input_data.txt')
-
-
 
 datatypes = data.keys()
 handle, aa_seq = io.readFasta(data['fasta_file'])
 
-
 ss_seq = matchSeq2SS(aa_seq, data['ss_file'])
 print ss_seq
 
-
-#ss_seq = io.readPsiPred(psipred_file)
+# ss_seq = io.readPsiPred(psipred_file)
 
 ss_def, ss_combi = ss.genSSCombinations(ss_seq)
-#ss_def, ss_combi = ss.genSSCombinations2(ss_seq)
+# ss_def, ss_combi = ss.genSSCombinations2(ss_seq)
 
 print ss_combi
 io.dumpPickle("ss_profiles.pickle", ss_combi)
@@ -96,7 +89,6 @@ io.dumpPickle("ss_profiles.pickle", ss_combi)
 # Read in contacts at a given confidence level
 if 'contacts_file' in datatypes:
     contacts, contacts_seq = io.readContacts(contactsfile, probability=0.7)
-
 
 native_pdbs = data['native_pdbs']
 
@@ -123,15 +115,9 @@ print rmsd_cutoff
 if (len(chisqr_cutoff) != 4) or (len(axrh_cutoff) != 4):
     print "The number of specified cutoffs should be exactly 4"
 
-
-
 clash_distance = float(data['clash_distance'])
 
 print 'clash_distance: ', clash_distance
-
-
-
-
 
 # read in PCS data from .npc file from Rosetta's broker file format
 pcsdata = io.getPcsTagInfo(ss_seq, data['pcs_broker'])
@@ -141,13 +127,12 @@ map_route = PCSmap.getRoute(ss_seq, pcsdata)
 print map_route, '1'
 
 io.dumpPickle("pcs_route.pickle", map_route)
-#io.dumpPickle("contact_route.pickle", rank_ss)
+# io.dumpPickle("contact_route.pickle", rank_ss)
 database_cutoff = data['database_cutoff']
 
 data_dict = {'ss_seq': ss_seq, 'pcs_data': pcsdata, 'aa_seq': aa_seq, 'natives': native_pdbs, \
-             'clash_distance':clash_distance, 'database_cutoff':database_cutoff, \
-             'axrh_cutoff': axrh_cutoff, 'chisqr_cutoff' : chisqr_cutoff, 'rmsd_cutoff': rmsd_cutoff}
-
+             'clash_distance': clash_distance, 'database_cutoff': database_cutoff, \
+             'axrh_cutoff': axrh_cutoff, 'chisqr_cutoff': chisqr_cutoff, 'rmsd_cutoff': rmsd_cutoff}
 
 io.dumpPickle("exp_data.pickle", data_dict)
 
@@ -159,19 +144,19 @@ for ss in ss_def:
     tres = ss[1]
     total_ss_res += tres
 
-fout = open("concat.txt",'w')
+fout = open("concat.txt", 'w')
 
-for i in range(0,len(map_route)):
+for i in range(0, len(map_route)):
     smotif = map_route[i]
-    if i ==0:
+    if i == 0:
         print smotif
         s1 = smotif[0]
         s2 = smotif[1]
         print s1, s2
-        #print ss_combi[s1][0][1],
-        #print ss_combi[s2][0][1]
-        res_sofar = ss_combi[s1][0][1]+ ss_combi[s2][0][1]
-        percent = (res_sofar/float(total_ss_res))*100
+        # print ss_combi[s1][0][1],
+        # print ss_combi[s2][0][1]
+        res_sofar = ss_combi[s1][0][1] + ss_combi[s2][0][1]
+        percent = (res_sofar / float(total_ss_res)) * 100
         print percent
         outline = 'Executable="mpirun -np 128 python stage1_mpi_run.py"\nrun="$Executable"\necho $run\n$run\n'
         print outline
@@ -181,66 +166,88 @@ for i in range(0,len(map_route)):
         if smotif[-1] == 'right':
             s2 = smotif[1]
             res_sofar = res_sofar + ss_combi[s2][0][1]
-            percent = (res_sofar/float(total_ss_res))*100
+            percent = (res_sofar / float(total_ss_res)) * 100
             print percent
             if i == 1:
-                num_of_models = (len(map_route)-i)*125
+                num_of_models = (len(map_route) - i) * 125
                 print num_of_models
-                outline = 'Executable="mpirun -np 128 python stage2_mpi_run.py '+str(num_of_models)+'"\nrun="$Executable"\necho $run\n$run\n'
+                outline = 'Executable="mpirun -np 128 python stage2_mpi_run.py ' + str(
+                    num_of_models) + '"\nrun="$Executable"\necho $run\n$run\n'
                 print outline
                 fout.write(outline)
                 continue
 
             if percent > 25.0 and percent < 50.0:
-                if i >=2:
-                    num_of_models = (len(map_route)-i)*125
+                if i >= 2:
+                    num_of_models = (len(map_route) - i) * 125
                     print "num_of_models", num_of_models
-                    outline = 'Executable="mpirun -np 128 python stage3x_mpi_run.py '+str(num_of_models)+'"\nrun="$Executable"\necho $run\n$run\n'
+                    outline = 'Executable="mpirun -np 128 python stage3x_mpi_run.py ' + str(
+                        num_of_models) + '"\nrun="$Executable"\necho $run\n$run\n'
+                    print outline
+                    fout.write(outline)
+                else:
+                    num_of_models = (len(map_route) - i) * 125
+                    print "num_of_models", num_of_models
+                    outline = 'Executable="mpirun -np 128 python stage3x_mpi_run.py ' + str(
+                        num_of_models) + '"\nrun="$Executable"\necho $run\n$run\n'
                     print outline
                     fout.write(outline)
             if percent > 50.0 and percent < 75.0:
-                num_of_models = (len(map_route)-i)*125
+                num_of_models = (len(map_route) - i) * 125
                 print "num_of_models", num_of_models
-                outline = 'Executable="mpirun -np 128 python stage3x_mpi_run.py '+str(num_of_models)+'"\nrun="$Executable"\necho $run\n$run\n'
+                outline = 'Executable="mpirun -np 128 python stage3x_mpi_run.py ' + str(
+                    num_of_models) + '"\nrun="$Executable"\necho $run\n$run\n'
                 print outline
                 fout.write(outline)
-            if percent > 75.0 :
-                num_of_models = (len(map_route)-i)*125
+            if percent > 75.0:
+                num_of_models = (len(map_route) - i) * 125
                 print "num_of_models", num_of_models
-                outline = 'Executable="mpirun -np 128 python stage4x_mpi_run.py '+str(num_of_models)+'"\nrun="$Executable"\necho $run\n$run\n'
+                outline = 'Executable="mpirun -np 128 python stage4x_mpi_run.py ' + str(
+                    num_of_models) + '"\nrun="$Executable"\necho $run\n$run\n'
                 print outline
                 fout.write(outline)
         else:
             s1 = smotif[0]
             res_sofar = res_sofar + ss_combi[s1][0][1]
-            percent = (res_sofar/float(total_ss_res))*100
+            percent = (res_sofar / float(total_ss_res)) * 100
             print percent
             if i == 1:
-                num_of_models = (len(map_route)-i)*125
+                num_of_models = (len(map_route) - i) * 125
                 print "num_of_models", num_of_models
-                outline = 'Executable="mpirun -np 128 python stage2_mpi_run.py '+str(num_of_models)+'"\nrun="$Executable"\necho $run\n$run\n'
+                outline = 'Executable="mpirun -np 128 python stage2_mpi_run.py ' + str(
+                    num_of_models) + '"\nrun="$Executable"\necho $run\n$run\n'
                 print outline
                 fout.write(outline)
                 continue
             if percent > 25.0 and percent <= 50.0:
-                if i >=2:
-                    num_of_models = (len(map_route)-i)*125
+                if i >= 2:
+                    num_of_models = (len(map_route) - i) * 125
                     print "num_of_models", num_of_models
-                    outline = 'Executable="mpirun -np 128 python stage2_mpi_run.py '+str(num_of_models)+'"\nrun="$Executable"\necho $run\n$run\n'
+                    outline = 'Executable="mpirun -np 128 python stage2_mpi_run.py ' + str(
+                        num_of_models) + '"\nrun="$Executable"\necho $run\n$run\n'
+                    print outline
+                    fout.write(outline)
+                else:
+                    num_of_models = (len(map_route) - i) * 125
+                    print "num_of_models", num_of_models
+                    outline = 'Executable="mpirun -np 128 python stage2_mpi_run.py ' + str(
+                        num_of_models) + '"\nrun="$Executable"\necho $run\n$run\n'
                     print outline
                     fout.write(outline)
             if percent > 50.0 and percent <= 75.0:
-                num_of_models = (len(map_route)-i)*125
+                num_of_models = (len(map_route) - i) * 125
                 print "num_of_models", num_of_models
-                outline = 'Executable="mpirun -np 128 python stage3x_mpi_run.py '+str(num_of_models)+'"\nrun="$Executable"\necho $run\n$run\n'
+                outline = 'Executable="mpirun -np 128 python stage3x_mpi_run.py ' + str(
+                    num_of_models) + '"\nrun="$Executable"\necho $run\n$run\n'
                 print outline
                 fout.write(outline)
-            if percent > 75.0 :
-                num_of_models = (len(map_route)-i)*125
+            if percent > 75.0:
+                num_of_models = (len(map_route) - i) * 125
                 print "num_of_models", num_of_models
-                outline = 'Executable="mpirun -np 128 python stage4x_mpi_run.py '+str(num_of_models)+'"\nrun="$Executable"\necho $run\n$run\n'
+                outline = 'Executable="mpirun -np 128 python stage4x_mpi_run.py ' + str(
+                    num_of_models) + '"\nrun="$Executable"\necho $run\n$run\n'
                 print outline
                 fout.write(outline)
 fout.close()
-run= 'cat submit0.sh concat.txt > submitX.sh'
+run = 'cat submit0.sh concat.txt > submitX.sh'
 os.system(run)
