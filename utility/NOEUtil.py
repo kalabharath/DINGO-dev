98ef3ffaa83a92a820046fe3fd0f85a3d020f224
kalabharath <kalabharath@gmail.com>
2017-05-03 15:36:33 -0600

    NOE plus tol

--- utility/NOEUtil.py
+++ utility/NOEUtil.py
@@ -42,7 +42,9 @@ def parseNOEData(noe_files):
         if len(noe) <= 1:
             pass
         else:
-            res1, atm1, res2, atom2, noe = noe.split()
+            res1, atm1, res2, atom2, noe, tol = noe.split()
             total_noe_count += 1.0
-            noe_data.append([int(res1), atm1, int(res2), atom2, float(noe)+0.5])
+            if float(tol) == 0.0:
+                tol = 0.1
+            noe_data.append([int(res1), atm1, int(res2), atom2, float(noe), float(tol)])
     return noe_data, total_noe_count
