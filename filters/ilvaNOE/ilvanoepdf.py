1d2a96ce84d2d0a22db4f01beddc7b425d717576
kalabharath <kalabharath@gmail.com>
2017-11-04 10:18:54 -0600

    scoring ILVA-NOEs

--- filters/ilvaNOE/ilvanoepdf.py
+++ filters/ilvaNOE/ilvanoepdf.py
@@ -327,8 +327,8 @@ def sX2ILVApdf(transformed_coors, native_sse_order, current_ss, sorted_noe_data,
     impossible_noes = copy.deepcopy(sorted_noe_data[1])
     noe_data = copy.deepcopy(sorted_noe_data[2])
     error_array = copy.deepcopy(sorted_noe_data[3])
-    max_noe_limit = 12.5
-    max_violations = 3
+    max_noe_limit = 7.5
+    max_violations = 100
     unsatisfied_noes = []
 
     noes_found = len(satisfied_noes)
