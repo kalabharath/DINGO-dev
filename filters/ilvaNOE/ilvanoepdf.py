e07247812d69fce0d915cf288e103459b42e6102
kalabharath <kalabharath@gmail.com>
2017-11-14 12:16:45 -0700

    Scoring ILVA-NOEs

--- filters/ilvaNOE/ilvanoepdf.py
+++ filters/ilvaNOE/ilvanoepdf.py
@@ -436,7 +436,7 @@ def sX2ILVApdf(transformed_coors, native_sse_order, current_ss, sorted_noe_data,
 
 
         count += 1.0
-        if count >= (len(smotif_noe_data) / 2.0):
+        if count >= (len(smotif_noe_data) / 3.0):
             tprob = noes_found / total_noes
             threshold = exp_data['expected_noe_prob'][stage - 1]
             #threshold = threshold - 0.1
