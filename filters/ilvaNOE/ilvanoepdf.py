d1f55e008ad345012732a69949c2a22baebff01f
kalabharath <kalabharath@gmail.com>
2017-11-14 12:15:28 -0700

    Scoring ILVA-NOEs

--- filters/ilvaNOE/ilvanoepdf.py
+++ filters/ilvaNOE/ilvanoepdf.py
@@ -440,8 +440,14 @@ def sX2ILVApdf(transformed_coors, native_sse_order, current_ss, sorted_noe_data,
             tprob = noes_found / total_noes
             threshold = exp_data['expected_noe_prob'][stage - 1]
             #threshold = threshold - 0.1
+            noeenergy = getNOEenergy(error_array, total_noes)
             if tprob < threshold:
-                return 0.001, noes_found, 0.00, [satisfied_noes, unsatisfied_noes, error_array], cluster_protons, cluster_sidechains
+                return 0.001, noes_found, 0.00, [satisfied_noes, unsatisfied_noes], cluster_protons, cluster_sidechains
+            elif noeenergy > 10.0:
+                return 0.001, noes_found, 0.00, [satisfied_noes, unsatisfied_noes], cluster_protons, cluster_sidechains
+            else:
+                pass
+
 
     noe_energy = numpy.sum(error_array)
     noe_energy = noe_energy/float(total_noes)
