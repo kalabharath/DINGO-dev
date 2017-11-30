1e3178b2cd3a9bf0d01a181284f3dd487665d588
kalabharath <kalabharath@gmail.com>
2017-11-30 07:51:40 -0700

    Scoring ILVA-NOEs

--- smotif_search.py
+++ smotif_search.py
@@ -141,7 +141,7 @@ def S1SmotifSearch(task):
         if 'reference_ca' in exp_data_types:
             ref_rmsd = ref.calcRefRMSD(exp_data['reference_ca'], s1_def, s2_def, smotif_data[i], rmsd_cutoff=100.0)
             tlog.append(['Ref_RMSD', ref_rmsd, seq_identity])
-            print "hit", ref_rmsd, noe_energy
+
         # Dump the data to the disk
         if pcs_tensor_fits or noe_probability:
             dump_log.append(tlog)
@@ -350,7 +350,7 @@ def sXSmotifSearch(task):
             print "Reducing the amount of data to:", rank_top_hits[stage - 1], len(dump_log)
         print "num of hits", len(dump_log),
         # io.dumpPickle("tx_" + str(index_array[0]) + "_" + str(index_array[1]) + ".pickle", dump_log)
-        #io.dumpGzipPickle("tx_" + str(index_array[0]) + "_" + str(index_array[1]) + ".gzip", dump_log)
+        io.dumpGzipPickle("tx_" + str(index_array[0]) + "_" + str(index_array[1]) + ".gzip", dump_log)
         return dump_log
     else:
         return False
